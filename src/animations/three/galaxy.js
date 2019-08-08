954146812d1b27e6d8879c5658f0f8e86b7df934
Liquid Solid <liquid.solid@protonmail.com>
2019-08-08 20:43:30 +0300

    Fix galaxy (micro)

--- src/animations/three/galaxy.js
+++ src/animations/three/galaxy.js
@@ -24,13 +24,6 @@ function velocityAngleEnergy (value) {
   return tween(0.7, 1, value)
 }
 
-// class Buffer {
-//   constructor (size) {
-
-//   }
-//   setDrawRange (count)
-// }
-
 class Star {
   constructor (energy) {
     this.vre = velocityRadiusEnergy(energy)
@@ -43,8 +36,6 @@ class Star {
     this.quaternion = new THREE.Quaternion()
     let angle = Math.PI / 2 * 0.5 + (Math.random() * 2 - 1) * Math.PI / 10
     this.quaternion.setFromAxisAngle(new THREE.Vector3(1, 0, 0), angle)
-    // this.appeared = false
-    // this.setColor()
   }
   coords () {
     let vec = new THREE.Vector3(
@@ -54,13 +45,6 @@ class Star {
     )
     vec.applyQuaternion(this.quaternion)
     return [vec.x, vec.y, vec.z]
-    // let zLevel = this.r / RADIUS
-    // let zTop = -(this.energy * 2 - 1) * 200
-    // return [
-    //   this.r * Math.cos(this.a),
-    //   this.r * Math.sin(this.a),
-    //   zTop * zLevel
-    // ]
   }
   rgb () {
     return new Array(3).fill(this.energy)
@@ -72,10 +56,6 @@ class Star {
     this.a += this.va * t * this.vae
     if (this.r <= 15) {
       this.reset()
-      // if (!this.appeared) {
-      //   this.appeared = true
-      //   return true
-      // }
     }
   }
   reset () {
@@ -83,7 +63,6 @@ class Star {
     this.r = RADIUS
     this.vr = 0
     this.va = ANGLE_VEL
-
   }
 }
 
@@ -111,31 +90,21 @@ export default class extends Animation {
     let material = new THREE.PointsMaterial({
       size: 3,
       color: 0xffffff
-      // vertexColors: THREE.VertexColors
     })
     let points = new THREE.Points(this.geometry, material)
     this.scene.add(points)
     this.camera.position.set(0, 0, 600)
-
-    // this.geometry.setDrawRange(0, 0)
-    // this.appeared = 0
   }
   animate (delta) {
     let t = delta * 0.001
     let { position } = this.geometry.attributes
     this.stars.forEach((x, i) => {
-      // let appeared = x.animate(t)
-      // if (appeared) {
-      //   this.appeared += 1
-      //   // console.log('appeared', this.appeared)
-      // }
       x.animate(t)
       x.coords().forEach((value, j) => {
         position.array[i * 3 + j] = value
       })
     })
     position.needsUpdate = true
-    // this.geometry.setDrawRange(0, this.appeared)
     this.render()
   }
 }
