934be1f77d8f1f0b0aa9c5a1df18e4fa96eea95b
Liquid Solid <liquid.solid@protonmail.com>
2020-04-21 10:05:21 +0300

    Компонент AppButton

---
+++ src/components/AppButton.vue
@@ -0,0 +1,33 @@
+<script>
+import { mergeData } from 'vue-functional-data-merge';
+
+export default {
+  name: 'AppButton',
+  functional: true,
+  render(h, context) {
+    return h('div', mergeData(context.data, {
+      staticClass: 'app-button',
+    }), context.children);
+  },
+};
+</script>
+
+<style lang="sass">
+@import '../assets/sass/const'
+@import '../assets/sass/easings'
+
+.app-button
+  border: none
+  background: $primary
+  color: $background
+  padding: 10px
+  font-size: 1em
+  cursor: pointer
+  transition: transform .15s $ease-out-back
+  &:focus
+    outline: none
+  &:hover
+    opacity: 0.9
+  &:active
+    transform: scale(0.95)
+</style>
