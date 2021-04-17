c355c82baf45ccb4eb0a01273150c65a81308936
Liquid <a.marcius26@gmail.com>
2021-04-17 15:20:57 +0300

    refactor looper

--- src/utils/use-loop-fn.ts
+++ src/utils/use-loop-fn.ts
@@ -1,9 +1,10 @@
-import { ref, Ref, watch, watchEffect } from 'vue';
+import { useRafFn } from '@vueuse/core';
+import { ref, Ref, watch } from 'vue';
 import { MaybeRef, normalizeMaybeRef } from './maybe-ref';
 
 export type LoopFn = (dt: number, elapsed: number) => void;
 
-function perfNow(): number {
+function performanceNow(): number {
     return window.performance.now();
 }
 
@@ -20,51 +21,46 @@ export default function useLoopFn(params: {
     // хранение ошибки
     const error = ref<Error | null>(null);
 
-    let lastFrame = perfNow();
+    let lastFrame = performanceNow();
     let elapsed = 0;
-    let request: number | null = null;
 
-    // запуск
-    function go() {
-        requestTick();
-    }
+    const { pause, resume } = useRafFn(
+        () => {
+            const now = performanceNow();
+            const delta = now - lastFrame;
+            lastFrame = now;
 
-    // остановка
-    function stop() {
-        if (request) {
-            cancelAnimationFrame(request);
-        }
-    }
-
-    function requestTick() {
-        request = requestAnimationFrame(tick);
-    }
-
-    // тикающая функция
-    function tick() {
-        const now = perfNow();
-        const delta = now - lastFrame;
-        lastFrame = now;
-
-        if (delta < 300) {
-            try {
-                elapsed += delta;
-                loopFn.value?.(delta, elapsed);
-            } catch (err) {
-                console.error('[loop error]', err);
-                error.value = err;
+            if (delta < 300) {
+                try {
+                    elapsed += delta;
+                    loopFn.value?.(delta, elapsed);
+                } catch (err) {
+                    console.error('[loop error]', err);
+                    error.value = err;
+                }
             }
-        }
-
-        requestTick();
-    }
-
-    // остановка или запуск в зависимости от состояния
-    watch(
-        () => !!loopFn.value && loopActive.value,
-        (isLoopShouldPlaying) => {
-            isLoopShouldPlaying ? go() : stop();
         },
+        { immediate: false },
+    );
+
+    watch(
+        [loopFn, loopActive],
+        ([fn, act], [oldFn, oldAct]) => {
+            if (fn !== oldFn) {
+                elapsed = 0;
+            }
+
+            if (fn && !oldFn) {
+                lastFrame = performanceNow();
+            }
+
+            if (fn && act) {
+                resume();
+            } else {
+                pause();
+            }
+        },
+        { immediate: true, flush: 'post' },
     );
 
     return {
