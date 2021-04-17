02444d6fc17f7b20f630c7ebe14f18a5f0d79b2e
Liquid <a.marcius26@gmail.com>
2021-04-17 08:18:26 +0300

    build: use windi

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import icons from 'vite-plugin-icons';
+import windi from 'vite-plugin-windicss';
 
 export default defineConfig({
-    plugins: [vue(), vueJsx()],
+    plugins: [vue(), vueJsx(), icons(), windi()],
     base: '/some-animations/',
     build: {
         // chunkSizeWarningLimit: 600,
